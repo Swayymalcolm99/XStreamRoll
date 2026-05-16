45cab953f8dd0257a10c3c37274715146b6a48c6
Xhr!st!n3 <hassymaya@gmail.com>
2026-05-16 06:53:59 +0000

    feat: add WebSocket streaming gateway (#106)

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common"
+import { GatewaysModule } from "./gateways/gateways.module"
 import { HealthController } from "./health/health.controller"
 import { TagsModule } from "./tags/tags.module"
 
 @Module({
-  imports: [TagsModule],
+  imports: [GatewaysModule, TagsModule],
   controllers: [HealthController],
   providers: [],
 })
